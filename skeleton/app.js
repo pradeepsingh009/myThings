c688642d2ff6da162161d92c50ddf87d4287e401
Pradeep Singh <pradeep.singh@happiestminds.com>
2018-01-02 17:01:20 +0530

    testing push

--- skeleton/app.js
+++ skeleton/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
+
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
