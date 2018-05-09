a23a33f3816bf47175aace42d0c848810d476db0
Pradeep Singh <pradeep.singh@happiestminds.com>
2018-05-09 18:55:27 +0530

    basic registration working

--- authproject/authapp/views.py
+++ authproject/authapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
 from authapp.forms.sign_up import SignUpForm
+import sys
 # Create your views here.
 
 
@@ -9,7 +11,19 @@ def sign_up(request):
     if(request.method == "POST"):
         sign_up_form = SignUpForm(request.POST)
         if(sign_up_form.is_valid):
-            sign_up_form.save()
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            email = request.POST.get('email')
+            first_name = request.POST.get('first_name')
+            last_name = request.POST.get('last_name')
+            user = User()
+            print(type(request.POST.get('email1')))
+            print(request.POST.get('email1'))
+            sys.exit(0)
+            user.set_password(password)
+            user.username = username
+            user.email = email
+            user.save()
 
     else:
         sign_up_form = SignUpForm()
