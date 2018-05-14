47abb3cbad06e661133e365bffef4a58dae903db
spradeepsinghsaini@gmail.com <spradeepsinghsaini@gmail.com>
2018-05-15 01:38:29 +0530

    media files working

--- authproject/authproject/urls.py
+++ authproject/authproject/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from authapp import views
-
+from django.conf.urls.static import static
+from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/',include('authapp.urls')),
     path('blog/',include('blog.urls'))
 ]
+
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+print(settings.INSTALLED_APPS)
