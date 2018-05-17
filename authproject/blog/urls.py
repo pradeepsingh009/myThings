71a9ac0c203bcaa22c891adb6bedeed18ff049fe
spradeepsinghsaini@gmail.com <spradeepsinghsaini@gmail.com>
2018-05-18 02:11:06 +0530

    templating done

--- authproject/blog/urls.py
+++ authproject/blog/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path,include
 from blog import views
 
+app_name = "blog"
+
 urlpatterns = [
-    path('',views.listing)
+    path('',views.listing),
+    path('<int:blog_id>',views.detail,name = "blog_detail")
 ]
 
--- authproject/blog/views.py
+++ authproject/blog/views.py
@@ -1,16 +1,68 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from blog.models import Article
+from blog.models import Article, ArticleCategory, ArticleCategoryJoin, Author
 from django.core.paginator import Paginator
+import sys
 # Create your views here.
+
 
 def listing(request):
     all_articles = Article.objects.all()
-    paginator = Paginator(all_articles,2)
+    paginator = Paginator(all_articles, 2)
 
     requestedPage = request.GET.get('page')
     articles = paginator.get_page(requestedPage)
+
+    # fetching all the categories
+    all_categories = ArticleCategory.objects.all()
+    categories_dict = {}
+    for category in all_categories:
+        article_count = ArticleCategoryJoin.objects.filter(category=category).count()
+        categories_dict[category.id] = {"category": category, "article_count": article_count}
+
+    # fetching all the authors
+    all_authors = Author.objects.all()
+    authors_dict = {}
+    for author in all_authors:
+        article_count = author.article_set.all().count()        
+        authors_dict[author.id] = {'author': author, "article_count": article_count}
+
     context = {
-        'articles' : articles
+        'articles': articles,
+        'categories_dict': categories_dict,
+        'authors_dict': authors_dict,
     }
-    return render(request,'blog/listing.html',context)
+    return render(request, 'blog/listing.html', context)
+
+
+def detail(request, blog_id):
+    all_articles = Article.objects.all()
+    paginator = Paginator(all_articles, 2)
+
+    # fetching all the categories
+    all_categories = ArticleCategory.objects.all()
+    categories_dict = {}
+    for category in all_categories:
+        article_count = ArticleCategoryJoin.objects.filter(category=category).count()
+        categories_dict[category.id] = {
+            "category": category, "article_count": article_count}
+
+    # fetching all the authors
+    all_authors = Author.objects.all()
+    authors_dict = {}
+    for author in all_authors:
+        article_count = author.article_set.all().count()
+        authors_dict[author.id] = {'author': author, "article_count": article_count}
+
+    try:
+        article = Article.objects.get(id=blog_id)
+
+        context = {
+            'article': article,
+            'categories_dict': categories_dict,
+            'authors_dict': authors_dict,
+        }
+
+        return render(request, 'blog/detail.html', context)
+    except Article.DoesNotExist:
+        return render(request, 'blog/article_not_found.html')
