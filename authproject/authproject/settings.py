54ba3b63fe7cfa64f86ae6e8edbe2b5f690ccea4
Pradeep Singh <pradeep.singh@happiestminds.com>
2018-05-18 19:32:46 +0530

    static working at both locations

--- authproject/authproject/settings.py
+++ authproject/authproject/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'djangoauth',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'root',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
@@ -129,6 +129,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static/blog"),
+# ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
